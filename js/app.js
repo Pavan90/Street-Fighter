38ca7c84f801d3d3130fd4a718e005fc2b988fc6
Pavan90 <pkodali@kent.edu>
2015-10-13 17:18:58 -0500

    removed conflicts in app.js

--- js/app.js
+++ js/app.js
@@ -29,62 +29,8 @@ $(document).ready(function() {
             $('.ryu-ready').show();
         });
 
-    /**************pressing key function assignment********************/
-    $(document).keydown(function(e) {
-    if (e.keyCode == 88) {
-      playCool();
-      $('.ryu-action').hide();
-      $('.ryu-cool').show();
-    }   
-  }).keyup(function(e) {
-    if (e.keyCode == 88) {
-      $('#cool')[0].pause();
-      $('#cool')[0].load();
-      $('.ryu-cool').hide();
-      $('.ryu-still').show();
-    }
-  });
-});
-
-var coolSound = false;
-function playCool () {
-  coolSound = !coolSound;
-  if (coolSound) {
-   $('#cool')[0].volume = 0.5;
-     //$('#cool')[0].load();
-    $('#cool')[0].play();
-  }
-}
-
-function playHadouken() {
-    $('#hadouken-sound')[0].volume = 0.5;
-    console.log('playHadouken()');
-    $('#hadouken-sound')[0].load();
-    $('#hadouken-sound')[0].play();
-
-}
-
-        })
-        .mousedown(function() {
-            playHadouken();
-            $('.ryu-ready').hide();
-            $('.ryu-throwing').show();
-            // $('.hadouken').show();
-            $('.hadouken').finish().show().animate({
-                    'left': '1020px'
-                },
-                500,
-                function() {
-                    $(this).hide();
-                    $(this).css('left', '520px');
-
-                }
-            );
-        })
-        .mouseup(function() {
-            $('.ryu-throwing').hide();
-            $('.ryu-ready').show();
-        });
+ 
+       
 /**************pressing key function assignment********************/
 $(document).keydown(function(keypressed) {
     if (keypressed.keyCode == 88) {
